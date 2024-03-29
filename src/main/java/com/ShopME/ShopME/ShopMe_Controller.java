e0a916788abe3732635f004f5afcd4259e35dc4a
Lemoupa-Frank <frankmichel022@gmail.com>
2024-03-29 11:19:10 +0100

    Added endpoint controller to add coupon

--- src/main/java/com/ShopME/ShopME/ShopMe_Controller.java
+++ src/main/java/com/ShopME/ShopME/ShopMe_Controller.java
@@ -1,13 +1,21 @@
 package com.ShopME.ShopME;
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.Parameter;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import models.Coupon;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import service.coupon_service;
 
 @RestController
 @RequestMapping("/ShopME")
 public class ShopMe_Controller {
+
+    public final coupon_service couponService;
+
+
+    @Autowired
+    public ShopMe_Controller(coupon_service couponService) {
+        this.couponService = couponService;
+    }
 
     @Operation(summary = "Home Page")
     @GetMapping("/")
@@ -26,5 +34,13 @@ public class ShopMe_Controller {
     public int fetchCoupons()
     {
         return 50;
+    }
+
+    @Operation(summary = "Admin Level add coupon")
+    @PostMapping("/addCoupons")
+    public Coupon addCoupon(@RequestBody Coupon coupon)
+    {
+        this.couponService.save_coupon(coupon);
+        return coupon;
     }
 }
