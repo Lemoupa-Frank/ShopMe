487f2e6ddfd208fd1c008bb829041473e3e9db97
Lemoupa-Frank <frankmichel022@gmail.com>
2024-03-31 22:22:15 +0100

    Linked to new methods in corresponding repository

--- src/main/java/com/ShopME/ShopME/service/transaction_service.java
+++ src/main/java/com/ShopME/ShopME/service/transaction_service.java
@@ -1,4 +1,24 @@
 package com.ShopME.ShopME.service;
 
+import com.ShopME.ShopME.Repository.TransactionRepository;
+import com.ShopME.ShopME.models.Transaction;
+import org.springframework.stereotype.Service;
+
+@Service
 public class transaction_service {
+    public final TransactionRepository transactionRepository;
+
+    public transaction_service(TransactionRepository transactionRepository) {
+        this.transactionRepository = transactionRepository;
+    }
+
+    public Transaction addtransaction(Transaction transaction)
+    {
+        return transactionRepository.save(transaction);
+    }
+
+    public Iterable<Transaction> getTransaction(String UserId, String OrderId)
+    {
+        return transactionRepository.findByUserIdAndOrderId(UserId,OrderId);
+    }
 }
