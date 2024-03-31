d786086415c9b679e40f2e2679b72ecdc02918d5
Lemoupa-Frank <frankmichel022@gmail.com>
2024-03-31 22:19:45 +0100

    added find method, find transaction by user Id and order Id

--- src/main/java/com/ShopME/ShopME/Repository/TransactionRepository.java
+++ src/main/java/com/ShopME/ShopME/Repository/TransactionRepository.java
@@ -4,7 +4,10 @@ import com.ShopME.ShopME.models.Transaction;
 import io.swagger.v3.oas.annotations.Hidden;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 @Hidden
 public interface TransactionRepository extends CrudRepository<Transaction, String> {
+    List<Transaction> findByUserIdAndOrderId(String userId, String orderId);
 
 }
