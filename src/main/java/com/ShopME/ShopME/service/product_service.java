0d35a7ef3c49e71cbf5d4dc5b1b5fc5dbee54f52
Lemoupa-Frank <frankmichel022@gmail.com>
2024-03-31 22:22:02 +0100

    Linked to new methods in corresponding repository

--- src/main/java/com/ShopME/ShopME/service/product_service.java
+++ src/main/java/com/ShopME/ShopME/service/product_service.java
@@ -1,4 +1,22 @@
 package com.ShopME.ShopME.service;
 
+import com.ShopME.ShopME.Repository.CouponRepository;
+import com.ShopME.ShopME.Repository.ProductRepository;
+import com.ShopME.ShopME.models.Product;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class product_service {
+    public final ProductRepository productrepository;
+
+    @Autowired
+    public product_service(ProductRepository productrepository) {
+        this.productrepository = productrepository;
+    }
+
+    public Iterable<Product> get_product()
+    {
+        return productrepository.findAll();
+    }
 }
