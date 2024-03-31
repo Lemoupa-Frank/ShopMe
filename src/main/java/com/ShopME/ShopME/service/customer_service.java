b8047906f79361f5319c89871d6d131aa134fb8d
Lemoupa-Frank <frankmichel022@gmail.com>
2024-03-31 22:21:31 +0100

    Linked to new methods in corresponding repository

--- src/main/java/com/ShopME/ShopME/service/customer_service.java
+++ src/main/java/com/ShopME/ShopME/service/customer_service.java
@@ -1,4 +1,21 @@
 package com.ShopME.ShopME.service;
 
+import com.ShopME.ShopME.Repository.CustomerRepository;
+import com.ShopME.ShopME.models.Customer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class customer_service {
+    public final CustomerRepository customerRepository;
+
+    @Autowired
+    public customer_service(CustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
+    }
+
+    public Customer create_account(Customer customer)
+    {
+        return customerRepository.save(customer);
+    }
 }
