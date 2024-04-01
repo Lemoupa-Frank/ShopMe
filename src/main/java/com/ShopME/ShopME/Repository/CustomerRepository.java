a19bcbe5dbf5472812e9ccbbf0f497f233136210
Lemoupa-Frank <frankmichel022@gmail.com>
2024-04-01 14:50:09 +0100

    added method to get all customers and update customer coupon

--- src/main/java/com/ShopME/ShopME/Repository/CustomerRepository.java
+++ src/main/java/com/ShopME/ShopME/Repository/CustomerRepository.java
@@ -2,10 +2,21 @@ package com.ShopME.ShopME.Repository;
 
 import com.ShopME.ShopME.models.Customer;
 import io.swagger.v3.oas.annotations.Hidden;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.transaction.annotation.Transactional;
 
 
 @Hidden
 public interface CustomerRepository extends CrudRepository<Customer, String> {
+    @Transactional
+    @Modifying
+    @Query("update Customer c set c.coupon_status = ?1, c.couponId = ?2 where c.userId = ?3")
+    int updateCoupon_statusAndCouponIdByUserId(String coupon_status, String couponId, String userId);
+    @Transactional
+    @Modifying
+    @Query("update Customer c set c.coupon_status = ?1 where c.userId = ?2")
+    int updateCoupon_statusByUserId(String coupon_status, String userId);
 
 }
