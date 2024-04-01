31b87d498a707d0163dea7d506685cc23a171106
Lemoupa-Frank <frankmichel022@gmail.com>
2024-04-01 08:52:02 +0100

    linked update product method from corresponding repository

--- src/main/java/com/ShopME/ShopME/service/product_service.java
+++ src/main/java/com/ShopME/ShopME/service/product_service.java
@@ -1,10 +1,11 @@
 package com.ShopME.ShopME.service;
 
-import com.ShopME.ShopME.Repository.CouponRepository;
 import com.ShopME.ShopME.Repository.ProductRepository;
 import com.ShopME.ShopME.models.Product;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class product_service {
@@ -18,5 +19,12 @@ public class product_service {
     public Iterable<Product> get_product()
     {
         return productrepository.findAll();
+    }
+
+    public void update_product(Long ordered, Long available){
+        productrepository.updateOrderedAndAvailableByProuductId(ordered,available,"pd0021");}
+
+    public Optional<Product> get_a_product(){
+        return productrepository.findById("pd0021");
     }
 }
