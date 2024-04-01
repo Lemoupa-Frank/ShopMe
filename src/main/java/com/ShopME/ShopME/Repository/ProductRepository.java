8eda3066248107d3e2baf655bb40fcf40e652178
Lemoupa-Frank <frankmichel022@gmail.com>
2024-04-01 08:50:44 +0100

    added update product methods

--- src/main/java/com/ShopME/ShopME/Repository/ProductRepository.java
+++ src/main/java/com/ShopME/ShopME/Repository/ProductRepository.java
@@ -2,9 +2,20 @@ package com.ShopME.ShopME.Repository;
 
 import com.ShopME.ShopME.models.Product;
 import io.swagger.v3.oas.annotations.Hidden;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.transaction.annotation.Transactional;
 
 @Hidden
 public interface ProductRepository extends CrudRepository<Product, String> {
+    @Transactional
+    @Modifying
+    @Query("update Product p set p.ordered = ?1, p.available = ?2 where p.prouductId = ?3")
+    int updateOrderedAndAvailableByProuductId(Long ordered, Long available, String prouductId);
+    @Transactional
+    @Modifying
+    @Query("update Product p set p.available = ?1 where p.prouductId = ?2")
+    int updateAvailableByProuductId(Long available, String prouductId);
 
 }
