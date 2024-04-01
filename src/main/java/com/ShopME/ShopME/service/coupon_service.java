994ab31e838576c8914daac1b5c639b2c934d5cb
Lemoupa-Frank <frankmichel022@gmail.com>
2024-04-01 14:48:20 +0100

    added apply for coupon and update coupon  functions

--- src/main/java/com/ShopME/ShopME/service/coupon_service.java
+++ src/main/java/com/ShopME/ShopME/service/coupon_service.java
@@ -5,6 +5,8 @@ import com.ShopME.ShopME.models.Coupon;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
 public class coupon_service {
     public final CouponRepository couponRepository;
@@ -19,5 +21,19 @@ public class coupon_service {
     }
     public Iterable<Coupon> getCoupons(){
         return couponRepository.findAll();
+    }
+
+    public int update_coupon_number(String coupon_id, Long quantity){ return couponRepository.updateCouponQuantityByCouponId(quantity,coupon_id);
+    }
+
+    /**
+     * Returns a coupon if there exist a coupon
+     * of that id
+     * @param id identifier of the coupon
+     * @return coupon if present else none
+     */
+    public Optional<Coupon> get_Coupon(String id)
+    {
+        return couponRepository.findById(id);
     }
 }
