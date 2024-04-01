6c7928ae10bad024f6a9c10ef36dc50cd0c1b6b8
Lemoupa-Frank <frankmichel022@gmail.com>
2024-04-01 14:48:53 +0100

    added get all customers from repository metthod

--- src/main/java/com/ShopME/ShopME/service/customer_service.java
+++ src/main/java/com/ShopME/ShopME/service/customer_service.java
@@ -5,6 +5,8 @@ import com.ShopME.ShopME.models.Customer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
 public class customer_service {
     public final CustomerRepository customerRepository;
@@ -18,7 +20,27 @@ public class customer_service {
     {
         return customerRepository.save(customer);
     }
-    public boolean Customer_Is_Real(String id){
-        return customerRepository.existsById(id);
+    public Optional<Customer> getCustomer(String id)
+    {
+        return customerRepository.findById(id);
+    }
+
+    /**
+     * Sets a coupon as used
+     * @param user_id the id of the customer
+     */
+    public void update_coupon_to_used(String user_id)
+    {
+        customerRepository.updateCoupon_statusByUserId("Used",user_id);
+    }
+
+    public void add_coupon(String user_id, String coupon_id)
+    {
+        customerRepository.updateCoupon_statusAndCouponIdByUserId("unused",coupon_id,user_id);
+    }
+
+    public Iterable<Customer> list_of_users()
+    {
+        return customerRepository.findAll();
     }
 }
